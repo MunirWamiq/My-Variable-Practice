c338f15b6a4b6d9bbf69b3ddd5fa7654050e436e
munir <munirbusiness619@gmail.com>
2024-03-04 22:56:59 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// My own practice
+console.log("Munir");
+var firstName = "Hania";
+console.log(firstName);
+var famousPerson = "Quaid e Azam";
+var quote = "\"All Pakistanis Are Equal\"";
+console.log("".concat(famousPerson, " said that:").concat(quote));
+var fName = "mahad";
+console.log(fName);
+var stripped = fName.trim();
+console.log(stripped);
+var practice = "\n\t Waseem \t\n";
+console.log(practice);
---
+++ main.ts
@@ -0,0 +1,13 @@
+// My own practice
+console.log("Munir");
+let firstName ="Hania";
+console.log(firstName);
+let famousPerson:string="Quaid e Azam";
+var quote = `"All Pakistanis Are Equal"`;
+console.log(`${famousPerson} said that:${quote}`);
+let fName = "mahad";
+console.log(fName);
+let stripped = fName.trim();
+console.log(stripped);
+let practice = "\n\t Waseem \t\n";
+console.log(practice);
